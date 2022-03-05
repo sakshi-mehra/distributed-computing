138cb3456d6c17b64d994ebbaab09aab2dff12e8
sakshi-mehra <sakshi.mehra@convosight.com>
2022-03-04 22:01:48 -0500

    Updated user controller to call the other containers on add user call

--- src/main/java/com/example/project/controller/UserController.java
+++ src/main/java/com/example/project/controller/UserController.java
@@ -3,13 +3,19 @@ package com.example.project.controller;
 import com.example.project.ProjectApplication;
 import com.example.project.entity.User;
 import com.example.project.service.Impl.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.http.HttpHeaders;
+import java.util.HashMap;
 import java.util.List;
+
 
 @Controller
 public class UserController {
@@ -36,6 +42,14 @@ public class UserController {
         if (ProjectApplication.IS_LEADER) {
             // Call other 2 service APIs
             // on any failure return
+            String uri_b = "http://localhost:8081/add";
+            String uri_c = "http://localhost:8082/add";
+            RestTemplate restTemplate = new RestTemplate();
+            User newUser = new User();
+            newUser.setFirstName(user.getFirstName());
+            newUser.setLastName(user.getLastName());
+            String result_b = restTemplate.getForObject(uri_b,String.class,newUser);
+            String result_c = restTemplate.getForObject(uri_c,String.class,newUser);
         }
 
         try {
